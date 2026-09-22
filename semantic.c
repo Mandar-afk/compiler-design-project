19168632b62f99e6185e8c14b6d3410c7348b64d
hazelbenny <hazelbenny06@gmail.com>
2026-09-22 20:45:37 +0530

    Update semantic.c

--- semantic.c
+++ semantic.c
@@ -1,46 +1,56 @@
 #include "compiler.h"
 
-char symbolTable[MAX_SYMBOLS][MAX_LEXEME];
-int symbolCount = 0;
+static char symbolTable[MAX_SYMBOLS][MAX_IDENTIFIER_LENGTH + 1];
+static int symbolCount = 0;
+static int semanticError = 0;
 
-void resetSymbolTable() {
+void resetSymbolTable(void) {
     symbolCount = 0;
+    semanticError = 0;
 }
 
 int isDeclared(const char *name) {
-
     for (int i = 0; i < symbolCount; i++) {
-
-        if (strcmp(symbolTable[i], name) == 0) {
-            return 1;
-        }
+        if (strcmp(symbolTable[i], name) == 0) return 1;
     }
-
     return 0;
 }
 
 int declareVariable(const char *name) {
-
     if (isDeclared(name)) {
-
-        printf("\nSemantic Error: Variable '%s' already declared\n", name);
+        fprintf(stderr, "Semantic Error: variable '%s' is already declared.\n", name);
+        semanticError = 1;
         return 0;
     }
 
-    strcpy(symbolTable[symbolCount], name);
-    symbolCount++;
+    if (symbolCount >= MAX_SYMBOLS) {
+        fprintf(stderr, "Semantic Error: symbol table limit reached.\n");
+        semanticError = 1;
+        return 0;
+    }
 
+    strncpy(symbolTable[symbolCount], name, MAX_IDENTIFIER_LENGTH);
+    symbolTable[symbolCount][MAX_IDENTIFIER_LENGTH] = '\0';
+    symbolCount++;
     return 1;
 }
 
-void printSymbolTable() {
+void setSemanticError(void) {
+    semanticError = 1;
+}
 
-    printf("\n===== SYMBOL TABLE =====\n");
+int hasSemanticError(void) {
+    return semanticError;
+}
 
-    printf("Variable\tType\n");
-    printf("----------------------\n");
-
+void printSymbolTable(void) {
+    printf("\nSYMBOL TABLE\n");
+    printf("------------------------\n");
+    if (symbolCount == 0) {
+        printf("(empty)\n");
+        return;
+    }
     for (int i = 0; i < symbolCount; i++) {
-        printf("%s\t\tint\n", symbolTable[i]);
+        printf("%d. %s : int\n", i + 1, symbolTable[i]);
     }
 }
